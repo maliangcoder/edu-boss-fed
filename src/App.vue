8680b2b9db1372d4b95e4c40d3a066ad00244bfd
maliang <maliangcoder@163.com>
2023-02-07 23:30:30 +0800

    (es-lint)css and global styles config

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,29 @@
     <h1>个人学习</h1>
     <!-- 根路由出口 -->
     <router-view />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <p class="test">hello world</p>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
 
+export default Vue.extend({
+
+})
+
+</script>
 <style lang="scss" scoped>
+// @import "~@/styles/variables.scss";
 
+.test {
+  color: $warning-color;
+}
 </style>
